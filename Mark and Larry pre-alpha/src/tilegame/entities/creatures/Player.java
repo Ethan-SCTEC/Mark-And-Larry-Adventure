a95b1dd67f0a5798ea977be75300d3277c860a50
JSteward5158 <94134571+JSteward5158@users.noreply.github.com>
2021-11-23 13:31:08 -0500

    Updated Movement
    Also updated the KeyManager class.

--- Mark and Larry pre-alpha/src/tilegame/entities/creatures/Player.java
+++ Mark and Larry pre-alpha/src/tilegame/entities/creatures/Player.java
@@ -1,10 +1,10 @@
 package tilegame.entities.creatures;
 
-import tilegame.Game;
 import tilegame.Handler;
 import tilegame.gfx.Assets;
 
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 public class Player extends Creature{
 
@@ -23,40 +23,41 @@ public class Player extends Creature{
 	private void getInput() {
 		xMove = 0;
 		yMove = 0;
+		int keyDown = handler.getKeyManager().getLastKeyHeld();
 		
-		int directionMoving = 0;
-		
-		
-		if(handler.getKeyManager().up) {
-			directionMoving = 1;
-		}
-		if(handler.getKeyManager().down) {
-			directionMoving = 2;
-		}
-		if(handler.getKeyManager().left) {
-			directionMoving = 3;
-		}
-		if(handler.getKeyManager().right) {
-			directionMoving = 4;
-		}
-		
-		switch(directionMoving) {
-		case 1:
-			yMove = -speed;
+
+		switch(keyDown) {
+		case KeyEvent.VK_UP:
+			yMove -= speed;
 			break;
-		case 2:
-			yMove = speed;
+		case KeyEvent.VK_DOWN:
+			yMove += speed;
 			break;
-		case 3:
-			xMove = -speed;
+		case KeyEvent.VK_LEFT:
+			xMove -= speed;
 			break;
-		case 4:
-			xMove = speed;
+		case KeyEvent.VK_RIGHT:
+			xMove += speed;
+			break;
+		case KeyEvent.VK_W:
+			yMove -= speed;
+			break;
+		case KeyEvent.VK_S:
+			yMove += speed;
+			break;
+		case KeyEvent.VK_A:
+			xMove -= speed;
+			break;
+		case KeyEvent.VK_D:
+			xMove += speed;
 			break;
 		default:
 			xMove = 0;
 			yMove = 0;
+			break;
+		
 		}
+		
 		
 	}
 	
